5fc893f12dde426c6b86222786ce50196c4090b0
Salem Mufarreh <salemmuf9@gmail.com>
2023-05-19 11:51:39 +0300

    adding spring

---
+++ src/test/java/Hospital/Management/System/REST/API/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Hospital.Management.System.REST.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
